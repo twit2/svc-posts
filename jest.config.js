8f4af727ebd04d420615a9196170fdcb867d7c89
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-12-10 23:25:20 +0100

    update workflow

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,18 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.ts"
+    ],
+    coveragePathIgnorePatterns: [
+        "node_modules",
+        "src/models",
+        "src/op",
+        "src/routes",
+        "src/types",
+        "src/rpc",
+        "src/Index.ts",
+        "src/PostsWorker.ts"
+    ]
 };
